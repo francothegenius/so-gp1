6531b969dabc8adb21aa6bcf91f80c80fd3f502d
Francisco Mariano Amézquita Ramos <a01634495@itesm.mx>
2019-05-07 01:59:24 +0000

    Práctica 5

--- thread/philosopher.c
+++ thread/philosopher.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <time.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
 #define NUM_PHILOSOPHERS 5
 #define NUM_FORKS 5
 
-semaphore_t forks[NUM_FORKS];
+
+sem_t forks[NUM_FORKS];
 
 
-long saldo = 1000;
-pthread_mutex_t lock;
+long randomInRange(int start, int final) {
+	return start + random() % (final - start);
+}
 
-void * printHello(void * td) {
-	long id = (long) td;
-	while(1){
+void * life(void * td) {
+	long id = (long)td;
+	
+	srandom((id +1) * time(NULL));
+
+	printf("Soy el filosofo %ld\n", id);
+	while(1) {
 		//pensar
-		if(td % 2 == 0){
-			sem_wait(&forks[id]); //decrementa el semaforo
-			sem_wait(&forks[id + 1] % NUM_PHILOSOPHERS); 
+		printf("%ld> voy a pensar\n", id);
+		sleep(randomInRange(5,10));
+		printf("%ld> Tengo hambre\n", id);
+		if(id % 2 == 0) {
+			sem_wait(&forks[id]); // decrementando el semaforo
+			sem_wait(&forks[(id+1)%NUM_PHILOSOPHERS]);
+		} else {
+			sem_wait(&forks[(id+1)%NUM_PHILOSOPHERS]);
+			sem_wait(&forks[id]); // decrementando el semaforo
 		}
-		else{
-			sem_wait(&forks[id + 1] % NUM_PHILOSOPHERS); 
-			sem_wait(&forks[id]); //decrementa el semaforo
-		}
-		//comer
+		
+		// comer
+		printf("%ld> Voy a comer\n", id);
+		sleep(randomInRange(2,6));
+		sem_post(&forks[id]);
+		sem_post(&forks[(id+1)%NUM_PHILOSOPHERS]);
 	}
 }
 
 int main() {
-	pthread_t threads[NUM_THREAD];
+	pthread_t philosophers[NUM_PHILOSOPHERS];
 	long t;
-	for (t=0; t< NUM_THREAD; t++) {
-		pthread_create(&threads[t], NULL, printHello, (void *) t);
+	for (t=0; t< NUM_FORKS; t++) {
+		sem_init(&forks[t], 0,1);
 	}
 	
-	for (t=0; t< NUM_THREAD; t++) {
-		pthread_join(threads[t], NULL);
+	for (t=0; t< NUM_PHILOSOPHERS; t++) {
+		pthread_create(&philosophers[t], NULL, life, (void *) t);
 	}
-
-	printf("Saldo final %ld \n", saldo);
+	
 	pthread_exit(NULL);
 }
